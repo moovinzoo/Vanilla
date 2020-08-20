1cade1429ccc58f5e33fcdf5a6a651c899929b5e
moovinzoo <moovinzoo@gmail.com>
2020-08-20 14:31:36 +0900

    Processing todo list

--- Inst_AddingJsToWebPage/greeting.js
+++ Inst_AddingJsToWebPage/greeting.js
@@ -1,27 +1,93 @@
-const form = document.querySelector(".js-form"),
-input = form.querySelector("input"),
-greeting = document.querySelector(".js-greetings");
+const form = document.querySelector(".js-greetings-form")
+,   input = form.querySelector("input")
+,   greeting = document.querySelector(".js-greetings-text")
 
-const USER_LS = "currentUser",
-SHOWING_CN = "showing";
+const USER_LS = "currentUser"
+,   SHOWING_CN = "showing";
 
-function paintGreeting(userName) {
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${userName}` 
+    greeting.innerText = `Hello, ${text}`;
 }
 
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     if (currentUser === null) {
-        // he/she is not 
+        askForName();
     } else {
-        paintGreeting(currentUser)
+        paintGreeting(currentUser);
     }
 }
+
 
 function init() {
     loadName();
 }
 
 init();
+
+
+
+
+
+
+// const form = document.querySelector(".js-form")
+// ,   input = form.querySelector("input")
+// ,   greeting = document.querySelector(".js-greetings");
+
+// const USER_LS = "currentUser"
+// ,   SHOWING_CN = "showing";
+
+// function paintGreeting(userName) {
+//     form.classList.remove(SHOWING_CN);
+//     greeting.classList.add(SHOWING_CN);
+//     greeting.innerText = `Hello ${userName}` 
+// }
+
+// function saveName(userName) {
+//     localStorage.setItem(USER_LS, userName);
+// }
+
+// function askForName() {
+//     form.classList.add(SHOWING_CN);
+//     form.addEventListener("submit", handleSubmit);
+// }
+
+// function handleSubmit(event) {
+//     event.preventDefault();
+//     const currentValue = input.value;
+//     paintGreeting(currentValue);
+//     saveName(currentValue);
+// }
+
+// function loadName() {
+//     const currentUser = localStorage.getItem(USER_LS);
+//     if (currentUser === null) {
+//         askForName();
+//     } else {
+//         paintGreeting(currentUser)
+//     }
+// }
+
+// function init() {
+//     loadName();
+// }
+
+// init();
---
+++ Inst_AddingJsToWebPage/todo.js
@@ -0,0 +1,45 @@
+const form_todo = document.querySelector(".js-")
+
+
+
+
+
+
+
+
+
+// const form_todo = document.querySelector(".js-toDoForm");
+// const input_todo = form_todo.querySelector("input");
+// const list_todo = document.querySelector(".js-toDoList");
+
+// const TODOS_LS = "toDos";
+// function loadToDos() {
+//     const toDos = localStorage.getItem(TODOS_LS);
+//     if (toDos !== null) {
+//     }
+// }
+
+// function paintToDo(text) {
+//     const li = document.createElement("li");
+//     const deleteBtn = document.createElement("button");
+//     deleteBtn.value = "❌";
+//     const span = document.createElement("span");
+//     span.innerText = text;
+//     li.appendChild(span);
+//     li.appendChild(deleteBtn);
+//     list_todo.appendChild(li);
+// }
+
+// function handleSubmit(event) {
+//     event.preventDefault();
+//     const currentValue = input_todo.value;
+//     paintToDo(currentValue);
+//     input_todo.value = null;
+// }
+
+// function init() {
+//     loadToDos;
+//     form_todo.addEventListener("submit", handleSubmit);
+// }
+
+// init();
